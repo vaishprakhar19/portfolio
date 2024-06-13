9af86575fc6e087fa6f8664ee85ac720eb39b10f
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-06-13 19:49:02 +0530

    asd

--- src/screens/Education.js
+++ src/screens/Education.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./education.css";
+import birlaLogo from "../resources/birla_logo.jpg";
+import RLBLogo from "../resources/rlb_logo.png";
 
 const Education = () => {
     return (
@@ -13,16 +15,20 @@ const Education = () => {
                     <div class="timeline-circle"></div>
                 </div>
                 <div class="timeline-component timeline-content">
+                    <img className="institute-logo" src={birlaLogo}/>
                     <p className='project-heading'>Birla Institute of Applied Sciences, Bhimtal</p>
                     <p className='project-detail'>Bachelor of Technology in Computer Science and Engineering</p>
                     <p className='project-date'>Oct 2021 - Aug 2025</p>
-                    <p className='project-detail'>I am current</p>
+                    <p className='project-detail'>I am currently pursuing B.Tech CSE and have completed 6 semesters</p>
+                    <p style={{color:"white", marginTop:"15px"}}>Grade: 80.40%</p>
                 </div>
                 <div class="timeline-component timeline-content">
-                    <p className='project-heading'>Birla Institute of Applied Sciences, Bhimtal</p>
-                    <p className='project-detail'>Bachelor of Technology in Computer Science and Engineering</p>
+                    <img className="institute-logo" src={RLBLogo}/>
+                    <p className='project-heading'>Rani Laxmi Bai Memorial School, Lucknow</p>
+                    <p className='project-detail'>CBSE(XII), All India Senior Secondary Examination</p>
                     <p className='project-date'>Oct 2021 - Aug 2025</p>
-                    <p className='project-detail'>Some Text</p>
+                    <p className='project-detail'>Physics, Chemistry, Mathematics with Computer Science and English</p>
+                    <p style={{color:"white", marginTop:"15px"}}>Grade: 92.60%</p>
                 </div>
 
                 <div class="timeline-middle">
@@ -38,10 +44,12 @@ const Education = () => {
                     <div class="timeline-circle"></div>
                 </div>
                 <div class="timeline-component timeline-content">
-                    <p className='project-heading'>Birla Institute of Applied Sciences, Bhimtal</p>
-                    <p className='project-detail'>Bachelor of Technology in Computer Science and Engineering</p>
+                    <img className="institute-logo" src={RLBLogo}/>
+                    <p className='project-heading'>Rani Laxmi Bai Memorial School, Lucknow</p>
+                    <p className='project-detail'>CBSE(X), All India Secondary Examination</p>
                     <p className='project-date'>Oct 2021 - Aug 2025</p>
-                    <p className='project-detail'>Some Text</p>
+                    <p className='project-detail'>Science, Social Science, Mathematics, Information Technology, Hindi  and English</p>
+                    <p style={{color:"white", marginTop:"15px"}}>Grade: 91.83%</p>
                 </div>
             </div>
         </div >
